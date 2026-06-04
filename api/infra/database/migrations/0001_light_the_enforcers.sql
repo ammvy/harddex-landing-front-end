300de0c729412e4d1330503eaedbed74209df62b
Victor-Lis <victorlisbronzo1@gmail.com>
2026-06-04 13:47:59 -0300

    feat: add more user styles

---
+++ api/infra/database/migrations/0001_light_the_enforcers.sql
@@ -0,0 +1,4 @@
+ALTER TYPE "public"."user_style_enum" ADD VALUE 'Remote work';--> statement-breakpoint
+ALTER TYPE "public"."user_style_enum" ADD VALUE 'File / Media';--> statement-breakpoint
+ALTER TYPE "public"."user_style_enum" ADD VALUE 'Mobility';--> statement-breakpoint
+ALTER TYPE "public"."user_style_enum" ADD VALUE 'Light travel';
--- api/infra/database/models/user.schema.ts
+++ api/infra/database/models/user.schema.ts
@@ -1,26 +1,30 @@
-import { pgTable, pgEnum, serial, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, pgEnum, serial, varchar } from "drizzle-orm/pg-core";
 
-export const userStyleEnum = pgEnum('user_style_enum', [
-  'BASIC',
-  'INTERMEDIATE',
-  'ADVANCED',
-  'GAMER',
-  'PROFESSIONAL',
+export const userStyleEnum = pgEnum("user_style_enum", [
+  "BASIC",
+  "INTERMEDIATE",
+  "ADVANCED",
+  "GAMER",
+  "PROFESSIONAL",
+  "Remote work",
+  "File / Media",
+  "Mobility",
+  "Light travel",
 ]);
 
-export const permissionEnum = pgEnum('permission_enum', [
-  'ADMIN',
-  'USER',
-  'CURATOR',
+export const permissionEnum = pgEnum("permission_enum", [
+  "ADMIN",
+  "USER",
+  "CURATOR",
 ]);
 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 255 }).notNull(),
-  email: varchar('email', { length: 255 }).notNull().unique(),
-  password: varchar('password', { length: 255 }).notNull(),
-  style: userStyleEnum('style').default('BASIC'),
-  permission: permissionEnum('permission').default('USER'),
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  style: userStyleEnum("style").default("BASIC"),
+  permission: permissionEnum("permission").default("USER"),
 });
 
 export type UserSelect = typeof users.$inferSelect;
