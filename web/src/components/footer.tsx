2943c6bc21df786eeb8ee7d6607ad01e364fcad4
Victor-Lis <victorlisbronzo1@gmail.com>
2026-06-04 21:17:59 -0300

    feat: init /login page

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -18,13 +18,13 @@ function Footer() {
           </span>
         </div>
         <div className="col-span-12 md:col-span-4 flex md:justify-center gap-5 text-[12px] lowercase">
-          <a href="#" className="hover:text-[#3D7FFF]">
+          <a href="#" className="hover:text-primary">
             instagram
           </a>
-          <a href="#" className="hover:text-[#3D7FFF]">
+          <a href="#" className="hover:text-primary">
             github
           </a>
-          <a href="#" className="hover:text-[#3D7FFF]">
+          <a href="#" className="hover:text-primary">
             discord
           </a>
         </div>
